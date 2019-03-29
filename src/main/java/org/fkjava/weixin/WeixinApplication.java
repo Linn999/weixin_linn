17dc936dd98ac52a233e6742aab8e0a427400271
Zai_Lin <a1459328686@163.com>
2019-03-29 21:42:44 +0800

    Json第二次

--- src/main/java/org/fkjava/weixin/WeixinApplication.java
+++ src/main/java/org/fkjava/weixin/WeixinApplication.java
@@ -1,8 +1,14 @@
 package org.fkjava.weixin;
 
+import org.fkjava.weixin.domain.InMessage;
+import org.fkjava.weixin.service.JsonRedisSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
@@ -15,6 +21,15 @@ public class WeixinApplication {
 		XmlMapper mapper = new XmlMapper();
 		return mapper;
 		
+	}
+	
+	@Bean
+	public RedisTemplate<String, ? extends InMessage> inMessageTemplate(
+			@Autowired RedisConnectionFactory connectionFactory){
+		RedisTemplate<String, ? extends InMessage> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+		template.setValueSerializer(new JsonRedisSerializer<InMessage>());
+		return template;
 	}
 	
 	
--- src/main/java/org/fkjava/weixin/controller/MessageReceiverController.java
+++ src/main/java/org/fkjava/weixin/controller/MessageReceiverController.java
@@ -1,12 +1,18 @@
 package org.fkjava.weixin.controller;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 import org.fkjava.weixin.domain.InMessage;
 import org.fkjava.weixin.service.MessageConvertHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,7 +32,8 @@ public class MessageReceiverController {
 	//这种属性注入，相当于XML文件中的<property>元素
 	@Autowired
 	private XmlMapper xmlMapper;
-	
+	@Autowired
+	private RedisTemplate<String, ? extends InMessage> inMessageTemplate;
 	//控制器里面必须有处理器方法（Handler Method）才能执行操作，才不会404
 	//处理GET请求，HTTP协议支持GET,PUT,DELETE等请求方式，都有对应的注解
 	@GetMapping
@@ -57,7 +64,31 @@ public class MessageReceiverController {
 			return "success";
 		}
 		LOG.debug("转换后的消息对象\n{}\n",inMessage);
+
+		
+		String channel ="linn_"+inMessage.getMsgType();
 		// 把消息丢入队列
+		//1.完成对象的序列化
+//		ByteArrayOutputStream bos =new ByteArrayOutputStream();//字节输出流
+//		ObjectOutputStream out = new ObjectOutputStream(bos);
+//		out.writeObject(inMessage);
+//		
+//		byte[] data=bos.toByteArray();
+	
+//		//2.把序列化后的对象放入队列里面
+//		inMessageTemplate.execute(new RedisCallback<InMessage>() {
+//
+//			@Override
+//			public InMessage doInRedis(RedisConnection connection) throws DataAccessException {
+//				
+//				 connection.publish(channel.getBytes(), data);
+//				return null;
+//			}
+//			
+//		});
+
+		inMessageTemplate.convertAndSend(channel, inMessage);
+		 
 		// 消费队列中的消息
 		// 产生客服消息
 
